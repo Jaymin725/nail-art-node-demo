3d605f60f0d154a5d8274d59d25ba2b220c5fba7
Jaymin Bhavsar <jayminbhavsar07@gmail.com>
2024-08-29 12:27:51 +0530

    added home page...

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => res.render("index"));
+
 app.use("/catagories", catagoryRouter);
 app.use("/arts", artRouter);
 
